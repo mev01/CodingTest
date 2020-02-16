b0d2f847a7c28d07cb903681f7f22c3d8d394eb8
DH <mevolicus@gmail.com>
2020-02-16 19:33:45 +0900

    baekjoon2902

---
+++ baekjoon2902.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	string sen;
+	cin >> sen;
+	int num;
+	num = sen.length();
+	for (int i = 0; i < num; i++)
+	{
+		if (sen[i] >= 'A' && sen[i] <= 'Z')
+			cout << sen[i];
+	}
+}
