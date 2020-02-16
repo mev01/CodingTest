6ec074d0b6ac39c41cffab91854a4f9472d78dd6
DH <mevolicus@gmail.com>
2020-02-16 19:39:14 +0900

    baekjoon1152

---
+++ baekjoon1152.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	string sen;
+	getline(cin, sen);
+	int num;
+	int cnt = 1;
+	num = sen.length();
+	for (int i = 0; i < num; i++)
+	{
+		if (i == 0)
+			if (sen[i] == ' ')
+				cnt--;
+		if (i == num - 1) {
+			if (sen[i] == ' ')
+				break;
+		}
+		if (sen[i] == ' ')
+			cnt++;
+	}
+	cout << cnt;
+}
