c8d6efe31c8ca3775606d846e6f328cb8ddbf2cf
DH <mevolicus@gmail.com>
2020-10-17 13:19:00 +0900

    201015

---
+++ 입출력/baekjoon11719.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	string a;
+	while (getline(cin, a))
+		cout << a << "\n";
+}
---
+++ 자료구조/baekjoon9012.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int n;
+	cin >> n;
+	cin.ignore();
+	for (int i = 0; i < n; i++) {
+		string str;
+		int stack = 0;
+		getline(cin, str);
+		int len = str.length();
+
+		for (int j = 0; j < len && stack >= 0; j++) {
+			stack += str[j] - '(' ? -1 : 1;
+		}
+		cout << (stack ? "NO" : "YES") << "\n";
+	}
+}
