ec7dd68ceb07d448e1eba70724686bd7beb50225
DH <mevolicus@gmail.com>
2020-10-23 23:42:02 +0900

    201023

---
+++ DP/baekjoon2011.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+long arr[5001] = { 1 };
+
+int main()
+{
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	string str;
+	int len;
+	cin >> str;
+	len = str.length();
+
+	for (int i = 1; i <= len; i++){
+		if (i == 1) {
+			int a = (str[i - 1] - '0');
+			if (a % 10 >= 1) arr[i] += arr[i - 1];
+			continue;
+		}
+		int a = (str[i - 2] - '0') * 10 + (str[i - 1] - '0');
+		if (a >= 10 && a <= 26) arr[i] += arr[i - 2];
+		if (a % 10 >= 1) arr[i] += arr[i - 1];
+		arr[i] %= 1000000;
+	}
+	cout << arr[len];
+}
---
+++ DP/baekjoon2225.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+long long arr[201];
+
+int main()
+{
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int N, K;
+	cin >> N >> K;
+
+	arr[0] = 1;
+	for (int i = 1; i <= K; i++){
+		for (int j = 0; j <= N; j++){
+			arr[j] = (arr[j] + arr[j - 1]) % 1000000000;
+		}
+	}
+	cout << arr[N];
+}
