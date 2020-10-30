fc39382271b569e76773767185c95b29b9c3888a
DH <mevolicus@gmail.com>
2020-10-30 17:02:14 +0900

    201030

---
+++ Sorting/baekjoon1377.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int n, max = 0;
+	cin >> n;
+	vector<pair<int, int>> v(n);
+	for (int i = 0; i < n; i++){
+		cin >> v[i].first;
+		v[i].second = i;
+	}
+	sort(v.begin(),v.end());
+	for (int i = 0; i < n; i++){
+		if (v[i].second - i > max) max = v[i].second - i;
+	}
+	cout << max+1;
+}
---
+++ Sorting/beakjoon11652.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std;
+long long arr[100000];
+
+int main()
+{
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++){
+		cin >> arr[i];
+	}
+	sort(arr, arr + n);
+
+	int cnt = 0, rem = 1;
+	long long ans = arr[0];
+	for (int i = 1; i < n; i++) {
+		if (arr[i] == arr[i - 1])
+			rem++;
+		else {
+			if (rem > cnt) {
+				cnt = rem;
+				ans = arr[i - 1];
+			}
+			rem = 1;
+		}
+	}
+	if (rem > cnt) {
+		cnt = rem;
+		ans = arr[n - 1];
+	}
+	cout << ans;
+}
