f6de93acc44ab2541206a96e92cb061d008d530d
DH <mevolicus@gmail.com>
2020-11-10 15:26:37 +0900

    201110

---
+++ Tree/baekjoon11725.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int node[100001] = { 0,1 };
+vector<int> v[100001];
+
+void Tree(int r) {
+	for (int i = 0; i < v[r].size(); i++){
+		if (node[v[r][i]])
+			v[r].erase(v[r].begin() + i--);
+		else
+			node[v[r][i]] = r;
+	}
+	for (int i = 0; i < v[r].size(); i++) {
+		Tree(v[r][i]);
+	}
+}
+
+int main()
+{
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int n, a, b;
+	
+	cin >> n;
+	for (int i = 0; i < n - 1; i++) {
+		cin >> a >> b;
+		v[a].push_back(b);
+		v[b].push_back(a);
+	}
+	Tree(1);
+	for (int i = 2; i <= n; i++){
+		cout << node[i] << '\n';
+	}
+}
---
+++ Tree/baekjoon1991.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+using namespace std;
+
+int Tree[26][2];
+
+void Preorder(int i) {
+	cout << char(i + 'A');
+	if (Tree[i][0] >= 0)
+		Preorder(Tree[i][0]);
+	if (Tree[i][1] >= 0)
+		Preorder(Tree[i][1]);
+}
+
+void Inorder(int i) {
+	if (Tree[i][0] >= 0)
+		Inorder(Tree[i][0]);
+	cout << char(i + 'A');
+	if (Tree[i][1] >= 0)
+		Inorder(Tree[i][1]);
+}
+
+void Postorder(int i) {
+	if (Tree[i][0] >= 0)
+		Postorder(Tree[i][0]);
+	if (Tree[i][1] >= 0)
+		Postorder(Tree[i][1]);
+	cout << char(i + 'A');
+}
+
+
+int main()
+{
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int n;
+	cin >> n;
+	while (n--){
+		char a, b, c;
+		cin >> a >> b >> c;
+		Tree[a - 'A'][0] = b - 'A';
+		Tree[a - 'A'][1] = c - 'A';
+	}
+	Preorder(0);
+	cout << "\n";
+	Inorder(0);
+	cout << "\n";
+	Postorder(0);
+}
