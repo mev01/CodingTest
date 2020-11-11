dae46d6fa8a29b6e44af53d56c4a0c54a10df143
DH <mevolicus@gmail.com>
2020-11-11 16:43:54 +0900

    201111

---
+++ Tree/baekjoon1167.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int dis[100001];
+vector<pair<int, int>> vt[100001];
+
+void DFS(int pas, int i) {
+	for (int j = 0; j < vt[i].size(); j++){
+		if(pas!= vt[i][j].first)
+			dis[vt[i][j].first] = vt[i][j].second + dis[i];
+	}
+	for (int j = 0; j < vt[i].size(); j++) {
+		if (pas != vt[i][j].first)
+			DFS(i, vt[i][j].first);
+	}
+}
+
+int main()
+{
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int n, num, a, b, max = 0;
+	cin >> n;
+	for (int i = 0; i < n; i++){
+		cin >> num >> a;
+		while (a!=-1){
+			cin >> b;
+			vt[num].push_back(make_pair(a, b));
+			cin >> a;
+		}
+	}
+	DFS(0, 1);
+	for (int i = 1; i <= n; i++){
+		if (dis[i] > max){
+			max = dis[i];
+			num = i;
+		}
+	}
+	dis[num] = 0;
+	DFS(0, num);
+	max = 0;
+	for (int i = 1; i <= n; i++)
+		if (dis[i] > max)
+			max = dis[i];
+	cout << max;
+}
