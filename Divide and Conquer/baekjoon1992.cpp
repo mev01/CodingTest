046e3aaa6c707bd06a1a4ba766533e989c045d89
DH <mevolicus@gmail.com>
2020-12-15 14:34:54 +0900

    201215

---
+++ Divide and Conquer/baekjoon1992.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+using namespace std;
+
+int n, map[65][65];
+
+bool check(int x1, int x2, int y1, int y2) {
+	int chk = map[x1][y1];
+	for (int i = x1; i <= x2; i++){
+		for (int j = y1; j <= y2; j++) {
+			if (chk != map[i][j]) return false;
+		}
+	}
+	return true;
+}
+
+void solve(int x1, int x2, int y1, int y2) {
+	int dis = (x2 - x1 + 1) / 2;
+	if(x1==x2)
+		cout << map[x1][y1];
+	else {
+		if (check(x1, x2, y1, y2)) cout << map[x1][y1];
+		else {
+			cout << "(";
+			solve(x1, x1 + dis - 1, y1, y1 + dis - 1);
+			solve(x1, x1 + dis - 1, y2 - dis + 1, y2);
+			solve(x2 - dis + 1, x2, y1, y1 + dis - 1);
+			solve(x2 - dis + 1, x2, y2 - dis + 1, y2);
+			cout << ")";
+		}
+	}
+}
+
+int main()
+{
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	string a;
+
+	cin >> n;
+	for (int i = 1; i <= n; i++){
+		cin >> a;
+		for (int j = 0; j < n; j++){
+			map[i][j + 1] = a[j] - '0';
+		}
+	}
+
+	solve(1, n, 1, n);
+}
---
+++ Divide and Conquer/baekjoon2447.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+using namespace std;
+
+int n, map[6562][6562];
+
+void solve(int x, int y, int dis) {
+	if (dis == 3) {
+		for (int i = x + dis / 3; i <= x + dis / 3 * 2 - 1; i++) {
+			for (int j = y + dis / 3; j <= y + dis / 3 * 2 - 1; j++) {
+				map[i][j]++;
+			}
+		}
+		return;
+	}
+	for (int i = 0; i < 3; i++){
+		for (int j = 0; j < 3; j++){
+			solve(x + dis/3 * i, y + dis/3 * j, dis / 3);
+		}
+	}
+}
+
+int main()
+{
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	cin >> n;
+	solve(1, 1, n);
+	for (int i = 1; i <= n; i++){
+		for (int j = 1; j <= n; j++){
+			if (!map[i][j]) cout << "*";
+			else cout << " ";
+		}
+		cout << "\n";
+	}
+}
