2aa7c005378f3c8b17c806a856e67c1a5a8d6f8a
DH <mevolicus@gmail.com>
2020-12-23 17:10:05 +0900

    201223

---
+++ Binary Search/baekjoon1654.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+long long K, N, answer, ma = 0;
+vector<long> v;
+
+void search(int min, int max) {
+	int mid = min + (max - min) / 2, cnt = 0;
+	if (min == mid) {
+		answer = mid;
+		return;
+	}
+	for (int j = 0; j < K; j++) {
+		cnt += v[j] / mid;
+		if (cnt > N) break;
+	}
+	if (cnt >= N) search(mid, max);
+	else search(min, mid);
+}
+
+int main()
+{
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	cin >> K >> N;
+	for (int i = 0; i < K; i++){
+		int a;
+		cin >> a;
+		v.push_back(a);
+		ma += a;
+	}
+	ma /= N;
+	search(1, ma+1);
+
+	cout << answer;
+}
