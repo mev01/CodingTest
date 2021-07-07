30351b743a1b372d940389da18b912c2707db89e
mev01 <mevolicus@gmail.com>
2021-07-07 23:10:19 +0900

    210707

--- BOJ/Binary Search/baekjoon1981.java
+++ BOJ/Binary Search/baekjoon1981.java
@@ -6,21 +6,19 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class baekjoon1981 {
-	static int N, ans;
+	static int N, ans, MAX, MIN;
 	static int[] dr = {-1, 0, 1, 0}, dc = {0, 1, 0, -1};
 	static int[][] arr;
 	static Queue<Pos> que;
 	static boolean[][] visited;
 	
 	static class Pos{
-		int r, c, max, min;
+		int r, c;
 
-		public Pos(int r, int c, int max, int min) {
+		public Pos(int r, int c) {
 			super();
 			this.r = r;
 			this.c = c;
-			this.max = max;
-			this.min = min;
 		}
 	}
 	
@@ -32,46 +30,57 @@ public class baekjoon1981 {
 		N = Integer.parseInt(br.readLine());
 		arr = new int[N][N];
 		
-		int max = 0;
-		int min = 200;
+		MAX = 0;
+		MIN = 200;
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < N; j++) {
 				int tmp = Integer.parseInt(st.nextToken());
 				
 				arr[i][j] = tmp;
-				if(tmp > max)  max = tmp;
-				else if(tmp < min) min = tmp;
+				if(tmp > MAX)  MAX = tmp;
+				else if(tmp < MIN) MIN = tmp;
 			}
 		}
 		
-		bSearch(0, max - min);
+		binarySearch(0, MAX - MIN);
 		
 		System.out.print(ans);
 	}
 
-	private static void bSearch(int s, int e) {
+	private static void binarySearch(int s, int e) {
 		if(s == e) {
 			ans = s;
 			return;
 		}
 		int mid = (s + e) / 2;
 		
-		if(movePossible(mid)) bSearch(s, mid);
-		else bSearch(mid + 1, e);
+		if(findMinMax(mid)) binarySearch(s, mid);
+		else binarySearch(mid + 1, e);
+	}
+	
+	private static boolean findMinMax(int mid) {
+		for (int i = MIN; i + mid <= MAX; i++) {
+			if(movePossible(i, i + mid)) return true;
+		}
+		
+		return false;
 	}
 
-	private static boolean movePossible(int mid) {
+	private static boolean movePossible(int min, int max) {
 		que = new LinkedList<Pos>();
 		visited = new boolean[N][N];
 		
-		que.offer(new Pos(0, 0, arr[0][0], arr[0][0]));
+		if(min > arr[0][0] || max < arr[0][0]) return false;
+		que.offer(new Pos(0, 0));
 		visited[0][0] = true;
 		
 		while(!que.isEmpty()) {
 			Pos pos = que.poll();
 			
-			if(pos.r == N - 1 && pos.c == N - 1) return true;
+			if(pos.r == N - 1 && pos.c == N - 1) {
+				return true;
+			}
 			
 			for (int i = 0; i < 4; i++) {
 				int nr = pos.r + dr[i];
@@ -79,22 +88,10 @@ public class baekjoon1981 {
 				
 				if(isOutOfMap(nr, nc)) continue;
 				if(visited[nr][nc]) continue;
-				
-				if(pos.max < arr[nr][nc]) {
-					if(arr[nr][nc] - pos.min > mid) continue;
-					
-					que.offer(new Pos(nr, nc, arr[nr][nc], pos.min));
-				}
-				else if(pos.min > arr[nr][nc]) {
-					if(pos.max - arr[nr][nc] > mid) continue;
-					
-					que.offer(new Pos(nr, nc, pos.max, arr[nr][nc]));
-				}
-				else {
-					que.offer(new Pos(nr, nc, pos.max, pos.min));
-				}
+				if(min > arr[nr][nc] || max < arr[nr][nc]) continue;
 				
 				visited[nr][nc] = true;
+				que.offer(new Pos(nr, nc));
 			}
 		}
 		
