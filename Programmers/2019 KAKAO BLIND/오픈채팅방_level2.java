c1f9c8b24f97e8b199a354d20be56708c010e9bc
mev01 <mevolicus@gmail.com>
2021-07-25 21:54:26 +0900

    210725

--- Programmers/2019 KAKAO BLIND/오픈채팅방_level2.java
+++ Programmers/2019 KAKAO BLIND/오픈채팅방_level2.java
@@ -7,7 +7,7 @@ class Solution {
         hash = new HashMap<String, String>();
         int len = record.length;
         
-        StringBuffer sb = new StringBuffer();
+        List<String> message = new LinkedList<>();
         StringTokenizer st;
         
         for(int i = 0; i < len; ++i){
@@ -18,10 +18,6 @@ class Solution {
             if(inst.equals("Enter")){
                 String name = st.nextToken();
                 hash.put(user, name);
-                sb.append(user).append("님이 들어왔습니다./");
-            }
-            else if(inst.equals("Leave")){
-                sb.append(user).append("님이 나갔습니다./");
             }
             else if(inst.equals("Change")){
                 String name = st.nextToken();
@@ -29,20 +25,19 @@ class Solution {
             }
         }
         
-        sb.setLength(sb.length() - 1);
-        String ans = sb.toString();
-        
-        for(String s : hash.keySet()){ //저장된 key값 확인
-            ans = ans.replaceAll(s, hash.get(s));
-        }
-        
-        st = new StringTokenizer(ans, "/");
-        len = st.countTokens();
-        
-        String[] answer = new String[len];
         for(int i = 0; i < len; ++i){
-            answer[i] = st.nextToken();
+            st = new StringTokenizer(record[i]);
+            String inst = st.nextToken();
+            String user = st.nextToken();
+            
+            if(inst.equals("Enter")){
+                String name = st.nextToken();
+                message.add(hash.get(user) + "님이 들어왔습니다.");
+            }
+            else if(inst.equals("Leave")){
+                message.add(hash.get(user) + "님이 나갔습니다.");
+            }
         }
-        return answer;
+        return message.toArray(new String[0]);
     }
 }
