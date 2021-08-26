07852d55659260e54cb0351188199d414f3db64d
mev01 <mevolicus@gmail.com>
2021-08-26 11:01:34 +0900

    210826

---
+++ Programmers/2021 KAKAO BLIND/신규_아이디_추천.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(String new_id) {
+        new_id = new_id.toLowerCase();
+        new_id = new_id.replaceAll("[^a-z0-9-_.]", "");
+        new_id = new_id.replaceAll("[.]+", ".");
+        new_id = new_id.replaceAll("^[.]", "").replaceAll("[.]$", "");
+        if(new_id.length() == 0) new_id = new_id.concat("a");
+        if(new_id.length() >= 16){
+            new_id = new_id.substring(0, 15);
+            new_id = new_id.replaceAll("[.]$", "");
+        }
+        while(new_id.length() <= 2) new_id = new_id + new_id.charAt(new_id.length() - 1);
+        
+        return new_id;
+    }
+}
