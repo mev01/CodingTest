dbca7bf78ec4f8f2132041f6fcf922388996931f
mev01 <mevolicus@gmail.com>
2021-11-15 22:44:29 +0900

    211115

--- Programmers/2020 KAKAO BLIND/외벽점검_level3.java
+++ Programmers/2020 KAKAO BLIND/외벽점검_level3.java
@@ -9,23 +9,4 @@ class Solution {
         
         return -1;
     }
-    
-    void calcOrder(int cnt){
-        if(cnt == 0){
-            calcLength();
-            return;
-        }
-        
-        for(int i = cnt; i >= 1; i--){
-            if(list.size() == 0 || i <= list.get(list.size() - 1)){
-                list.add(i);
-                calcOrder(cnt - i);
-                list.remove(list.size() - 1);
-            }
-        }
-    }
-    
-    void calcLength(){
-        
-    }
 }
