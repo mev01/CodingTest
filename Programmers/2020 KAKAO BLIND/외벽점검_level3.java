c2b6ac7873170629a06fb2cac0e57be77db049cf
mev01 <mevolicus@gmail.com>
2021-11-25 23:35:00 +0900

    211125

--- Programmers/2020 KAKAO BLIND/외벽점검_level3.java
+++ Programmers/2020 KAKAO BLIND/외벽점검_level3.java
@@ -1,12 +1,76 @@
 import java.util.*;
 
 class Solution {
-    static ArrayList<Integer> list;
+    static int N, ans = Integer.MAX_VALUE;
+    static int[] weakArr, distArr, weakLine;
+    static ArrayList<Integer> friends;
+    static boolean[] subsetArr;
     
     public int solution(int n, int[] weak, int[] dist) {
-        list = new ArrayList<Integer>();
-        calcOrder(dist.length);
+        N = n;
+        weakArr = weak;
+        distArr = dist;
         
-        return -1;
+        weakLine = new int[weakArr.length * 2 - 1];
+        makeWeakToLine();
+        
+        subsetArr = new boolean[dist.length];
+        Subset(0);
+        
+        return ans;
+    }
+    
+    void makeWeakToLine(){
+        for(int i = 0; i < weakLine.length - 1; i++){
+            if(i >= weakArr.length) weakLine[i] = weakLine[i - weakArr.length] + N;
+            else weakLine[i] = weakArr[i];
+        }
+    }
+    
+    void Subset(int idx){
+        if(idx == distArr.length){
+            friends = calcFriendsToPutIn();
+            if(canInspection()){
+                ans = Math.min(ans, friends.size());
+            }
+            
+            return;
+        }
+        
+        subsetArr[idx] = false;
+        Subset(idx + 1);
+        
+        subsetArr[idx] = true;
+        Subset(idx + 1);
+    }
+    
+    ArrayList<Integer> calcFriendsToPutIn(){
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        for(int i = 0; i < subsetArr.length; i++){
+            if(subsetArr[i]){
+                list.add(distArr[i]);
+            }
+        }
+        
+        return list;
+    }
+    
+    Boolean canInspection(){
+        for(int i = 0; i < weakArr.length; i++){
+            int dist = i;
+            
+            for(int j = 0; j < friends.size(); j++){
+                int num = weakLine[dist] + friends.get(j);
+                
+                int idx = Arrays.binarySearch(weakLine, num);
+                if(idx < 0) idx = (idx + 1) * -1;
+                
+                if(idx >= i + weakArr.length) return true;
+                dist = idx;
+            }
+        }
+        
+        return false;
     }
 }
