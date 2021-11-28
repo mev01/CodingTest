fe32e8dcc0eec41277656aeeb8227aea7dd632a0
mev01 <mevolicus@gmail.com>
2021-11-28 23:38:50 +0900

    211128

--- Programmers/2018 KAKAO BLIND/[3차]n진수게임_level3.java
+++ Programmers/2018 KAKAO BLIND/[3차]n진수게임_level3.java
@@ -4,19 +4,19 @@ class Solution {
     public String solution(int n, int t, int m, int p) {
         StringBuilder sb = new StringBuilder();
         
-        for(int i = 0; i < n; i++){
-            int st = p + i * m;
+        for(int i = 0; i < t; i++){
+            int st = p - 1 + i * m;
             System.out.println("st " + st);
             
-            int s = 1, e = st;
+            int s = 0, e = st;
             while(s < e){
                 int M = (s + e) / 2, temp = M;
                 
                 int digit = 0;
-                while(temp > 0){
+                do{
                     temp /= n;
                     digit++;
-                }
+                }while(temp > 0);
                 System.out.println("M " + M);
                 System.out.println("digit " + digit);
                 
@@ -26,11 +26,11 @@ class Solution {
                     if(j != 1) cnt -= (j * (int)Math.pow(n, j - 1));
                     System.out.println("cnt " + cnt);
                 }
-                cnt += (M - (int)Math.pow(n, digit - 1)) * digit;
+                cnt += (M - ((int)Math.pow(n, digit - 1))) * digit;
                 System.out.println("cnt " + "end " + cnt);
                 
-                if(cnt < st) e = M - 1;
-                else s = M;
+                if(cnt >= st) e = M;
+                else s = M + 1;
             }
             
             System.out.println(s);
