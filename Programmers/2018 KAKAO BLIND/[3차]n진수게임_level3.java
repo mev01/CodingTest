841b449a8ce2736b95b38bda97d2b8d59cbaa974
mev01 <mevolicus@gmail.com>
2021-11-29 20:59:25 +0900

    211129

--- Programmers/2018 KAKAO BLIND/[3차]n진수게임_level3.java
+++ Programmers/2018 KAKAO BLIND/[3차]n진수게임_level3.java
@@ -5,38 +5,45 @@ class Solution {
         StringBuilder sb = new StringBuilder();
         
         for(int i = 0; i < t; i++){
-            int st = p - 1 + i * m;
-            System.out.println("st " + st);
+            int st = p + i * m;
             
             int s = 0, e = st;
             while(s < e){
-                int M = (s + e) / 2, temp = M;
+                int M = (s + e) / 2;
+                int num = getNumbers(n, M);
                 
-                int digit = 0;
-                do{
-                    temp /= n;
-                    digit++;
-                }while(temp > 0);
-                System.out.println("M " + M);
-                System.out.println("digit " + digit);
-                
-                int cnt = 0;
-                for(int j = 1; j <= digit - 1; j++){
-                    cnt += (j * (int)Math.pow(n, j));
-                    if(j != 1) cnt -= (j * (int)Math.pow(n, j - 1));
-                    System.out.println("cnt " + cnt);
-                }
-                cnt += (M - ((int)Math.pow(n, digit - 1))) * digit;
-                System.out.println("cnt " + "end " + cnt);
-                
-                if(cnt >= st) e = M;
+                if(num >= st) e = M;
                 else s = M + 1;
             }
             
-            System.out.println(s);
+            int remainNum = st - getNumbers(n, s - 1);
+            StringBuilder sToNum = new StringBuilder();
+            do{
+                if(s % n >= 10)  sToNum.append((char)(s % n - 10 + 'A'));
+                else sToNum.append(s % n);
+                s /= n;
+            }while(s > 0);
+            sToNum = sToNum.reverse();
+            sb.append(sToNum.charAt(remainNum - 1));
         }
         
-        String answer = "";
-        return answer;
+        return sb.toString();
+    }
+    
+    int getNumbers(int n, int M){
+        int temp = M, digit = 0;
+        do{
+            temp /= n;
+            digit++;
+        }while(temp > 0);
+
+        int cnt = 0;
+        for(int j = 1; j <= digit - 1; j++){
+            if(j != 1) cnt += j * (int)Math.pow(n, j - 1) * (n - 1);
+            else cnt += j * (int)Math.pow(n, j);
+        }
+        cnt += ((digit == 1 ? M + 1 : M) - ((int)Math.pow(n, digit - 1)) + 1) * digit;
+        
+        return cnt;
     }
 }
